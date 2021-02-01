0fa848ea5cff1cb653771fdfc8976b017664e3e5
cober2019 <53579603+cober2019@users.noreply.github.com>
2021-02-01 11:54:51 -0800

    Add files via upload

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ from decouple import config
 class Config(object):
 
     basedir = os.path.abspath(os.path.dirname(__file__))
+    UPLOAD_FOLDER = f'\\app\\uploads\\'
 
     # Set up the App SECRET_KEY
     SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_007')
